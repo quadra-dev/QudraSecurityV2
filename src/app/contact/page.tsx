36d07eb473daab39689df566800f3c60ac69ca8b
aryan raj <aryanraj.cams@gmail.com>
2025-10-11 22:35:22 +0530

    contact page faq updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,12 +10,37 @@ export default function Contact() {
           Frequently Asked Questions
         </h2>
         <div className=" flex flex-col items-center justify-center  p-6">
-          <FaqCard question="question" answer="answer" />
-          <FaqCard question="question" answer="answer" />
-          <FaqCard question="question" answer="answer" />
-          <FaqCard question="question" answer="answer" />
-          <FaqCard question="question" answer="answer" />
-          <FaqCard question="question" answer="answer" />
+          <FaqCard
+            question=" How does Quadra Security’s CCTV solution operate?"
+            answer="Our CCTV systems integrate HD/IP cameras, NVRs, and cloud connectivity to provide real-time 
+                    monitoring and smart analytics. Footage can be accessed remotely via secure mobile or desktop 
+                    dashboards for complete situational awareness. "
+          />
+          <FaqCard
+            question=" Can CCTV be integrated with other security systems? "
+            answer="Yes! We specialize in integrating CCTV with access control, alarm systems, and ANPR cameras for 
+                    unified security management and event-based automation.r"
+          />
+          <FaqCard
+            question="How do biometric systems enhance operational efficiency?"
+            answer="Our systems automate attendance tracking, eliminate proxy access, and generate instant reports — 
+                    improving workforce accountability and administrative accuracy. "
+          />
+          <FaqCard
+            question="Can ANPR data be used for automation and record-keeping? "
+            answer="Yes! The data seamlessly integrates with toll, parking, and gate management systems for automated 
+                    entry, billing, and audit trails."
+          />
+          <FaqCard
+            question="Do you offer remote maintenance or upgrades? "
+            answer="Absolutely! Our team provides remote diagnostics, firmware updates, and priority on-site support to 
+                    ensure uninterrupted communication. "
+          />
+          <FaqCard
+            question=" Do you provide complete installation and support services?"
+            answer="Absolutely! Quadra Security provides end-to-end services including delivery, installation, 
+                    configuration, and after-sales maintenance to ensure smooth operation and long-term reliability."
+          />
         </div>
       </div>
     </div>
