b9497b6df0de96aa79383e880b189459fa4baaf9
Krishal <krishalbatra18@gmail.com>
2025-10-19 21:50:02 +0530

    blog issue

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -12,7 +12,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "inhpcizi",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2025-01-01',
   useCdn: process.env.NODE_ENV === 'production'
