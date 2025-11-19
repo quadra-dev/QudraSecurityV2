ebb45a71b0aa5cab91cfa28f9106b00d1a0a7506
aryan raj <aryanraj.cams@gmail.com>
2025-11-19 22:46:25 +0530

    google verification code added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,58 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavigationBar from "@/components/navigation/NavigationBar"
-import Head from "next/head";
+import NavigationBar from "@/components/navigation/NavigationBar";
 import Footer from "@/components/Footer/Footer";
 import { Toaster } from "sonner";
 
 const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-inter", // will hook into Tailwind
+  variable: "--font-inter",
   display: "swap",
 });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <Head>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "Organization",
-            "name": "Quadra Security",
-            "url": "https://www.quadrasecurity.com",
-            "logo": "https://www.quadrasecurity.com/logo.png",
-            "description": "Quadra Security provides advanced security solutions including CCTV, surveillance, access control, and more.",
-            "sameAs": [
-              "https://www.linkedin.com/company/quadra-security",
-              "https://www.instagram.com/quadra_security/"
-            ],
-            "contactPoint": {
-              "@type": "ContactPoint",
-              "telephone": "+91-8708187957",
-              "contactType": "Customer Support",
-              "areaServed": "IN",
-              "availableLanguage": "English"
-            }
-          }),
-        }}
-      />
-    </Head>
-      <body className={`${inter.variable} overflow-x-hidden text-theme-white  bg-gradient-to-b from-[#1d002e] via-[#2e0152] to-[#4b0082]`}>
-        <NavigationBar/>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="4SZyundM086TBwsSBgTjzQ2x29phDN6SJFUuDhb87WY"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              name: "Quadra Security",
+              url: "https://www.quadrasecurity.com",
+              logo: "https://www.quadrasecurity.com/logo.png",
+              description:
+                "Quadra Security provides advanced security solutions including CCTV, surveillance, access control, and more.",
+              sameAs: [
+                "https://www.linkedin.com/company/quadra-security",
+                "https://www.instagram.com/quadra_security/",
+              ],
+              contactPoint: {
+                "@type": "ContactPoint",
+                telephone: "+91-8708187957",
+                contactType: "Customer Support",
+                areaServed: "IN",
+                availableLanguage: "English",
+              },
+            }),
+          }}
+        />
+      </head>
+
+      <body
+        className={`${inter.variable} overflow-x-hidden text-theme-white bg-gradient-to-b from-[#1d002e] via-[#2e0152] to-[#4b0082]`}
+      >
+        <NavigationBar />
         {children}
         <Footer />
         <Toaster position="top-right" richColors />
